a415db8cbe833179a852e57c236fbc922f4e972d
stevpet <peter.stevens@bakerhughes.com>
2014-01-16 13:34:18 +0100

    Added argumentparser, works

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tfsblame;
+
+namespace BHI.JewelSuite.tools.tfsblame.unittests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InvocationAsExpectedShouldGetFile()
+        {
+            ArgumentParser parser = new ArgumentParser();
+            var expected = "john.cs";
+            String[] arguments = {"/noprompt", expected};
+            var actual = parser.getFile();
+            Assert.AreEqual(expected,actual);
+        }
+
+        [TestMethod]
+        public void InvocationWithJustFilenameShouldGetFile()
+        {
+            ArgumentParser parser = new ArgumentParser();
+            var expected = "john.cs";
+            String[] arguments = { expected };
+            var actual = parser.getFile();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ tfsblame/ArgumentParser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Hosting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tfsblame
+{
+    public class ArgumentParser
+    {
+        private string file;
+        public void Parse(String[] arguments)
+        {
+            foreach (var argument in arguments)
+            {
+                if (!argument.Equals("/noprompt"))
+                {
+                    file = argument;
+                }
+            }
+        }
+        public String getFile()
+        {
+            return file;
+        }
+    }
+}
